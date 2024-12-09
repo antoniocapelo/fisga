cc94e04a2e42251513d1600d2abb3479b725ad5e
Capelo <capelo@pop-os.localdomain>
2024-12-09 08:09:17 +0000

    feat: pass config as param

--- src/commands/default.ts
+++ src/commands/default.ts
@@ -1,36 +1,27 @@
-import {Args, Command, Flags} from '@oclif/core'
+import {Args, Command} from '@oclif/core'
 import * as fs from 'fs'
-import * as path from 'path'
 import { select } from '@inquirer/prompts'
-import { executeCommand } from '../utils/executeCommand.js'
 import { interpretCommand } from '../utils/interpretCommand.js'
 
 export default class DefaultCommand extends Command {
-//   static args = {
-//     person: Args.string({description: 'Person to say hello to', required: true}),
-//   }
+  static args = {
+    config: Args.string({
+      description: 'Path to config file',
+      required: true,
+    }),
+  }
 
-  static description = 'Some default command'
+  static description = 'Run commands from a config file'
 
   static examples = [
-    `<%= config.bin %> <%= command.id %> friend --from oclif
-hello friend from oclif! (./src/commands/hello/index.ts)
-`,
+    '<%= config.bin %> path/to/config.json',
   ]
 
-//   static flags = {
-//     from: Flags.string({char: 'f', description: 'Who is saying hello', required: true}),
-//   }
-
   async run(): Promise<void> {
-    const {args, flags} = await this.parse(DefaultCommand)
+    const {args} = await this.parse(DefaultCommand)
 
     // Read config file
-    const configPath = path.join(
-      path.dirname(new URL(import.meta.url).pathname),
-      'config.json'
-    )
-    const configData = JSON.parse(fs.readFileSync(configPath, 'utf8'))
+    const configData = JSON.parse(fs.readFileSync(args.config, 'utf8'))
 
     // Present task options to user
     const selectedTask: any = await select({
