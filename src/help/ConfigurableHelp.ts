6670051aa7f896df132d9024ba7193f41367a8b2
Capelo <capelo@iMac-de-Capelo.local>
2025-01-13 10:10:30 +0000

    fix: fix help

--- src/help/ConfigurableHelp.ts
+++ src/help/ConfigurableHelp.ts
@@ -6,14 +6,31 @@ import { getHelpForPath } from '../findCommand.js'
 
 export default class ConfigurableHelp extends Help {
   async showHelp(argv: string[]) {
-    // Check for config file in arguments
-    const configArg = argv[1]
-    const remainingArgs = Array.from(argv).slice(2).filter(e => e !== '-h' && e !== '--help');
-    const argsPath = remainingArgs.join('.').replaceAll(' ', '.').replaceAll(':', '.')
+    // Find config argument using --config flag
+    const configArgIndex = argv.findIndex(arg => arg.startsWith('--config'))
+    let configPath: string | undefined
+    
+    if (configArgIndex !== -1) {
+      const configArg = argv[configArgIndex]
+      if (configArg === '--config') {
+        // Handle --config path-to-config format
+        configPath = argv[configArgIndex + 1]
+        // Remove both the flag and path from argv
+        argv.splice(configArgIndex, 2)
+      } else {
+        // Handle --config=path-to-config format
+        configPath = configArg.split('=')[1]
+        // Remove the combined flag from argv
+        argv.splice(configArgIndex, 1)
+      }
+    }
 
-    if (configArg) {
+    const remainingArgs = Array.from(argv).filter(e => e !== '-h' && e !== '--help');
+    const argsPath = remainingArgs.slice(1).join('.').replaceAll(' ', '.').replaceAll(':', '.')
+
+    if (configPath) {
       try {
-        const config = JSON.parse(fs.readFileSync(path.resolve(configArg), 'utf8'))
+        const config = JSON.parse(fs.readFileSync(path.resolve(configPath), 'utf8'))
         // Customize help output based on config
         // You can override various Help methods here
         return this.showCustomHelp(config, argsPath)
